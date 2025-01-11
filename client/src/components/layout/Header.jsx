53873533fa82ec5211e59ae46e8c817840013e8a
Joeeazy <joeeazy34@gmail.com>
2025-01-11 16:31:34 +0300

    header section update

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import { SearchBar } from "./SearchBar";
 import { NotificationBell } from "./NotificationBell";
 import { UserProfile } from "./UserProfile";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header className="h-16 bg-white border-b border-gray-200 fixed top-0 right-0 left-64 z-10">
       <div className="px-6 flex items-center justify-between">
         <SearchBar />
+        
         <div className="flex items-center gap-4">
           <NotificationBell count={3} />
+          <Link to={"/profile"} className="no-underline">
           <UserProfile />
+          </Link>
         </div>
+        
+        
       </div>
     </header>
   );
